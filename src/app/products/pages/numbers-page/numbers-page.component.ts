81fdc3b84834bef46cfb31d56f1b66b54a7389bc
Sergio Orejarena <sergioorueda@gmail.com>
2024-04-13 16:59:25 -0500

    Numerical Pipes

--- src/app/products/pages/numbers-page/numbers-page.component.ts
+++ src/app/products/pages/numbers-page/numbers-page.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-numbers-page',
-  templateUrl: './numbers-page.component.html',
-  styleUrl: './numbers-page.component.css'
+	selector: "app-numbers-page",
+	templateUrl: "./numbers-page.component.html",
+	styleUrl: "./numbers-page.component.css",
 })
 export class NumbersPageComponent {
-
+	public totalSells: number = 4576457.3456;
+	public percent: number = 0.4562;
 }
